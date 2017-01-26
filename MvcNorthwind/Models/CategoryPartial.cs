8ad9afcc4a94668c5ed22c8a163765f36b59eb35
Shane Jackson <A00966815@ad.bcit.ca>
2017-01-26 08:37:02 -0800

    first commit

---
+++ MvcNorthwind/Models/CategoryPartial.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNorthwind.Models.Northwind
+{
+    [MetadataType(typeof(CategoryMetaData))]
+    public partial class Category { }
+    public class CategoryMetaData
+    {
+        [Required]
+        [Display(Name = "Category")]
+        public object CategoryName { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 5)]
+        public object Description { get; set; }
+    }
+}
